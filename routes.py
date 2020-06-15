f69088d21f6708951281b93b9940a531cc29d1b8
Tomasz Szyczmak <255721@student.pwr.edu.pl>
2020-06-15 17:51:43 +0200

    Ehh

---
+++ routes.py
@@ -0,0 +1,19 @@
+from app import app
+from flask import render_template, url_for, redirect
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template("index.html")
+
+@app.route('/login')
+def login():
+    return render_template("login.html"
+
+@app.route('/register')
+def register():
+    return render_template("register.html"
+
+@app.route('/profile')
+def profile():
+    return render_template("profile.html"
